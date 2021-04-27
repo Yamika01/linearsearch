c26ac28d2ba9603cd6a297b235e10fc3b06067f7
yamikaganji <83218365+yamikaganji@users.noreply.github.com>
2021-04-27 18:23:14 +0530

    linearsearch using functions

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,30 +1,48 @@
 import java.util.Scanner;
 
-public class LinearSearch {
+public class Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int list[] = { 10, 20, 30, 40, 50, 60, 70 };
-		System.out.println("this is a linear search program");
-		Scanner sc =new Scanner(System.in);
-		System.out.print("enter the element to search:");
-		int n=sc.nextInt();
-		int isFound=0;//
-		
-			for (int i = 0; i < list.length; i++) 
-			{
-				if (list[i] == n) 
-				{
-					isFound=1;
-					System.out.println("the element is found at" +i +"position");
-				}	
-			}
-			if(isFound==0)
-			{
-				System.out.println("the element is not found");
-			}
-			
+
+		System.out.println("enter the size of array:");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int numbers[] = new int[size];
+		System.out.println("enter the elements in the array:");
+		for (int index = 0; index < numbers.length; index++)
+		{
+			numbers[index] = sc.nextInt();
+		}
 		
 
+		System.out.println("this is a linear search program");
+
+		System.out.print("enter the element to search:");
+		int n = sc.nextInt();
+		int position = linearSearch(numbers, n);
+		// System.out.println(position);
+		if (position == -1)
+		{
+			System.out.println("the number is not found");
+		}
+		else
+		{
+			System.out.println("the number is found at " + position + " position");
+		}
 	}
+
+	public static int linearSearch(int[] array, int ele) {
+
+		for (int i = 0; i < array.length; i++)
+		{
+			if (array[i] == ele) {
+
+				return i;
+			}
+		}
+
+		return -1;
+
+	}
+
 }
