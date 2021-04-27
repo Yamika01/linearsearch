aebf8586f2e5a8795d6b15960b58715ef276aef7
yamikaganji <83218365+yamikaganji@users.noreply.github.com>
2021-04-27 17:08:58 +0530

    added  logic for dynamic input

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,22 +1,30 @@
+import java.util.Scanner;
 
 public class LinearSearch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int list[]= {10,20,30,40,50,60,70};
-		int n=40;
-		System.out.println("the given list is:");
-		for(int i=0;i<list.length;i++) {
-			System.out.println(list[i]);
-			
-	}
-		for(int i=0;i<list.length;i++)
-		{
-			if(list[i]==n)
+		int list[] = { 10, 20, 30, 40, 50, 60, 70 };
+		System.out.println("this is a linear search program");
+		Scanner sc =new Scanner(System.in);
+		System.out.print("enter the element to search:");
+		int n=sc.nextInt();
+		int isFound=0;
+		
+			for (int i = 0; i < list.length; i++) 
 			{
-				System.out.println("the element is present at"+""+i);
+				if (list[i] == n) 
+				{
+					isFound=1;
+					System.out.println("the element is found at" +i +"position");
+				}	
 			}
-		}
+			if(isFound==0)
+			{
+				System.out.println("the element is not found");
+			}
+			
+		
 
-}
+	}
 }
